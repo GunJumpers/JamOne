b9b93c72e80b5052979d51a8484aa67dd88f09a6
Thrisha Kopula <thrishak1@gmail.com>
2022-10-11 14:00:03 -0700

    added player shrinking and doors

--- Assets/Scripts/MazeNoteCollision.cs
+++ Assets/Scripts/MazeNoteCollision.cs
@@ -7,10 +7,20 @@ public class MazeNoteCollision : MonoBehaviour
     public int collectNumber = 0;
     public AudioClip ButtonHit;
     public GameObject winObject;
+    public GameObject mazeBlocks;
+    public GameObject exWall;
+    public AK.Wwise.Event musicNote1 = null;
+    public AK.Wwise.Event musicNote2 = null;
+    public AK.Wwise.Event musicNote3 = null;
 
     private void Start()
     {
         winObject.SetActive(false);
+        mazeBlocks.SetActive(true);
+        exWall.SetActive(true);
+        musicNote1.Post(gameObject);
+        musicNote2.Post(gameObject);
+        musicNote3.Post(gameObject);
     }
 
     private void OnTriggerEnter(Collider collision) {
@@ -22,11 +32,19 @@ public class MazeNoteCollision : MonoBehaviour
             Debug.Log("collided");
         }
 
+        if (collision.gameObject.CompareTag("shrinkDetect"))
+        {
+            //shrink player or unshrink
+            gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            Debug.Log("shrunk");
+        }
+
         if (collectNumber == 3)
         {
             Debug.Log("YOU WIN");
             winObject.SetActive(true);
+            mazeBlocks.SetActive(false);
+            exWall.SetActive(false);
         }
     }
-
 }
